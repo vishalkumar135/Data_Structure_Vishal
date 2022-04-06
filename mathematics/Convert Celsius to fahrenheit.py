83d42dc0f92b6f0bd78a89d34275644b36ee62f6
vishal_kumar135 <kumarasvishal@gmail.com>
2022-04-06 22:26:30 +0530

    adding new file of mathmatics

---
+++ mathematics/Convert Celsius to fahrenheit.py
@@ -0,0 +1,4 @@
+celsius_temperature =  input("please enter the celsius_temperature")
+celsius_temperature = float(celsius_temperature)
+fahrenheit = (celsius_temperature*1.8)+32
+print('%.2f Celsius is equivalent to: %.2f Fahrenheit'%(celsius_temperature,fahrenheit))
