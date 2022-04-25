f045cbeaf9d9b75c9d235e260437d35c68e8b96d
vishal_kumar135 <kumarasvishal@gmail.com>
2022-04-25 08:21:01 +0530

    simple interest

--- Language_practice/find_the_simplest_interest.py
+++ Language_practice/find_the_simplest_interest.py
@@ -2,4 +2,5 @@ principal =float(input("enter the principal amount"))
 time =float(input("enter the time "))
 rate= float(input("enter the rate "))
 simple_interest=(principal*time*rate)/100 
-print("the simple interest is =",simple_interest)
+print("the simple interest is =",simple_interest)print("the simple interest is =",simple_interest)simple_interest=(principal*time*rate)/100
+print("the simple interest is %f =",simple_interest)
